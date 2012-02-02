35eeca4ae044d484432aa09bc89a055cb871dd0d
Byron Alleman <Byron Alleman@129.252.139.74>
2012-02-02 15:16:44 -0500

    Added all the activities I could think of.

---
+++ src/com/processfast/csce492/AddAssignmentTypeActivity.java
@@ -0,0 +1,13 @@
+package com.processfast.csce492;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddAssignmentTypeActivity extends Activity{
+	@Override 
+    public void onCreate(Bundle savedInstanceState) { 
+          super.onCreate(savedInstanceState); 
+          this.setContentView(R.layout.addassignmenttype);  
+    } 
+
+}
