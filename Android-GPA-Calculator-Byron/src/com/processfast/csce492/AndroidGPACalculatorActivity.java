a913f9a7e637fe04f05544624a97004766db3ce4
Brad Stancel <brad@processfast.com>
2012-02-04 13:42:18 -0500

    Committing Byron's cloned repo to master
    Testing using the Github app instead of the command line.

---
+++ Android-GPA-Calculator-Byron/src/com/processfast/csce492/AndroidGPACalculatorActivity.java
@@ -0,0 +1,26 @@
+package com.processfast.csce492;
+
+/**
+ * Starting activity file for Group Project
+ * Members: Brad Stancel, Byron Alleman,
+ * Shane Hoover, Vitaliy Zheltov and Stephen Strohier
+ * 
+ * CSCE 492
+ * Beginning date: 1/24/2012
+ * 
+ */
+
+//This is Brad commenting code from Shane's setup
+//Brad Rocks!
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidGPACalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ Android-GPA-Calculator-Byron/src/com/processfast/csce492/DeleteAssignmentTypeActivity.java
@@ -0,0 +1,12 @@
+package com.processfast.csce492;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeleteAssignmentTypeActivity extends Activity {
+	@Override 
+    public void onCreate(Bundle savedInstanceState) { 
+          super.onCreate(savedInstanceState); 
+          this.setContentView(R.layout.deleteassignmenttype);  
+    } 
+}
