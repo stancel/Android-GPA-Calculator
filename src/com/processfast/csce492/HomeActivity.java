e73c684c2e0f7cf348c3f1f11965ade10b414fa7
Byron Alleman <byron.alleman@gmail.com>
2012-02-06 23:20:15 -0500

    Added more database code.  Also, created the user interface for the AddCourseActivity activity and fixed an issue with the xml for the home screen that was making it impossible to compile and run.

--- src/com/processfast/csce492/HomeActivity.java
+++ src/com/processfast/csce492/HomeActivity.java
@@ -1,12 +1,35 @@
 package com.processfast.csce492;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
+	
+	private View.OnClickListener addCourseListener = new View.OnClickListener(){
+		@Override
+		public void onClick(View v) {
+			Intent i = new Intent(v.getContext(), AddCourseActivity.class);
+			startActivity(i);
+			
+		}
+		
+	};
+	
 	@Override 
     public void onCreate(Bundle savedInstanceState) { 
           super.onCreate(savedInstanceState); 
           this.setContentView(R.layout.home);  
+          
+          Button addCourse = (Button)findViewById(R.id.bAddCourse);
+          
+          addCourse.setOnClickListener(addCourseListener);
+          
     } 
+	
+	
 }
