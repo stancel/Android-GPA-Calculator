769117c1ddd9df4aeaf91603480440c81cdbbf47
Byron Alleman <byron.alleman@gmail.com>
2012-02-11 17:44:52 -0500

    Add Course Activity, still needs some work though.

--- src/com/processfast/csce492/Course.java
+++ src/com/processfast/csce492/Course.java
@@ -7,18 +7,16 @@ public class Course {
 	private String teacher;
 	private int hours;
 	private int style;
-	private int a;
-	private int a_minus;
-	private int b_plus;
-	private int b;
-	private int b_minus;
-	private int c_plus;
-	private int c;
-	private int c_minus;
-	private int d_plus;
-	private int d;
-	private int d_minus;
+	private GradingScale scale;
 	
+	public Course(){
+		dept = "";
+		number = "";
+		teacher = "";
+		hours = -1;
+		style = -1;
+		scale = new GradingScale();
+	}
 	public int getId() {
 		return id;
 	}
@@ -55,74 +53,15 @@ public class Course {
 	public void setStyle(int style) {
 		this.style = style;
 	}
-	public int getA() {
-		return a;
-	}
-	public void setA(int a) {
-		this.a = a;
-	}
-	public int getA_minus() {
-		return a_minus;
-	}
-	public void setA_minus(int a_minus) {
-		this.a_minus = a_minus;
-	}
-	public int getB_plus() {
-		return b_plus;
-	}
-	public void setB_plus(int b_plus) {
-		this.b_plus = b_plus;
-	}
-	public int getB() {
-		return b;
-	}
-	public void setB(int b) {
-		this.b = b;
-	}
-	public int getB_minus() {
-		return b_minus;
-	}
-	public void setB_minus(int b_minus) {
-		this.b_minus = b_minus;
-	}
-	public int getC_plus() {
-		return c_plus;
-	}
-	public void setC_plus(int c_plus) {
-		this.c_plus = c_plus;
-	}
-	public int getC() {
-		return c;
-	}
-	public void setC(int c) {
-		this.c = c;
-	}
-	public int getC_minus() {
-		return c_minus;
-	}
-	public void setC_minus(int c_minus) {
-		this.c_minus = c_minus;
-	}
-	public int getD_plus() {
-		return d_plus;
-	}
-	public void setD_plus(int d_plus) {
-		this.d_plus = d_plus;
-	}
-	public int getD() {
-		return d;
-	}
-	public void setD(int d) {
-		this.d = d;
-	}
-	public int getD_minus() {
-		return d_minus;
-	}
-	public void setD_minus(int d_minus) {
-		this.d_minus = d_minus;
-	}
+	
 	@Override
 	public String toString(){
-		return this.id + " " + this.dept+" "+this.number;
+		return this.id + " " + this.dept+" "+this.number + " " + this.teacher + " " + this.style;
+	}
+	public GradingScale getScale() {
+		return scale;
+	}
+	public void setScale(GradingScale scale) {
+		this.scale = scale;
 	}
 }
