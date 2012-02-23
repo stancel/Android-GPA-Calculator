5d7b4cb014314c026e2b3fd0a570f42c5b504aea
Vitaliy Zheltov <vzup21@gmail.com>
2012-02-23 15:34:43 -0500

    I finished the AddGradeActivity for now. I am pretty sure it has some logic errors at the moment, I will fix those a little later when I understand the database stuff a little better. I am also going to add better comments and put in the error notification stuff.

--- src/com/processfast/csce492/AddGradeActivity.java
+++ src/com/processfast/csce492/AddGradeActivity.java
@@ -12,9 +12,8 @@ import android.view.View.OnClickListener;
 import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
 
+public class AddGradeActivity extends Activity implements OnClickListener {
 
-public class AddGradeActivity extends Activity implements OnClickListener{
-	
 	private CoursesDataSource coursesSource;
 	private AssignmentTypesSource typeSource;
 	private GradesDataSource gradeSource;
@@ -22,79 +21,94 @@ public class AddGradeActivity extends Activity implements OnClickListener{
 	Spinner spCourse, spType;
 	EditText etName, etGradeNum, etGradeDen;
 	String selectedName, selectedCourse, selectedType;
-	Long selectedNum, selectedDen, finalGrade;
+	float numGrade, max_grade;
+	int courseID, typeID;
+	Grade grade = new Grade();
+	Course course = new Course();
+	AssignmentType type = new AssignmentType();
 	
-	@Override 
-    public void onCreate(Bundle savedInstanceState) { 
-          super.onCreate(savedInstanceState); 
-          this.setContentView(R.layout.addgrade); 
-          init();
-          
-          coursesSource = new CoursesDataSource(this);
-          coursesSource.open();
-          List<Course> courses = coursesSource.getAllCourses();
-          
-          int size = courses.size();
-          
-          ArrayAdapter <CharSequence> adapter1 =
-        		  new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
-        		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        		 
-          for(int i = 0 ; i < size ; i ++ ){
-        	  adapter1.add(courses.get(i).toString());
-          }
-          
-          typeSource = new AssignmentTypesSource(this);
-          typeSource.open();    
-          
-          final ArrayAdapter <CharSequence> adapter2 =
-        		new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
-        		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.addgrade);
+		init();
 
-          spCourse.setAdapter(adapter1);
-          spCourse.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			public void onItemSelected(AdapterView adapter1, View v, int i, long lng) {		
+		coursesSource = new CoursesDataSource(this);
+		coursesSource.open();
+		List<Course> courses = coursesSource.getAllCourses();
+
+		int size = courses.size();
+
+		ArrayAdapter<CharSequence> adapter1 = new ArrayAdapter<CharSequence>(
+				this, android.R.layout.simple_spinner_item);
+		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		for (int i = 0; i < size; i++) {
+			adapter1.add(courses.get(i).toString());
+		}
+
+		typeSource = new AssignmentTypesSource(this);
+		typeSource.open();
+
+		final ArrayAdapter<CharSequence> adapter2 = new ArrayAdapter<CharSequence>(
+				this, android.R.layout.simple_spinner_item);
+		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		spCourse.setAdapter(adapter1);
+		spCourse.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			public void onItemSelected(AdapterView adapter1, View v, int i,
+					long lng) {
 				selectedCourse = adapter1.getItemAtPosition(i).toString();
-				List<AssignmentType> types = typeSource.getAllAssignmentTypesForCourse(course_id);
+
+				List<AssignmentType> types = typeSource
+						.getAllAssignmentTypesForCourse(courseID);
 				int size2 = types.size();
-				
-		          for(int j = 0 ; j < size2 ; j ++ ){
-		        	  adapter2.add(types.get(j).toString());
-		          }
-		          spType.setAdapter(adapter2);
-		          spType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-		  			public void onItemSelected(AdapterView adapter2, View v, int i, long lng) {
+
+				for (int j = 0; j < size2; j++) {
+					adapter2.add(types.get(j).toString());
+				}
+				spType.setAdapter(adapter2);
+				spType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+					public void onItemSelected(AdapterView adapter2, View v,
+							int i, long lng) {
 						selectedType = adapter2.getItemAtPosition(i).toString();
-		  			}
-					public void onNothingSelected(AdapterView arg0) {
-						// TODO I think nothing goes here				
 					}
-		          });
+
+					public void onNothingSelected(AdapterView arg0) {
+						// TODO I think nothing goes here
+					}
+				});
 			}
+
 			public void onNothingSelected(AdapterView arg0) {
-				// TODO I think nothing goes here				
-			}       	  
-          });
-    }
+				// TODO I think nothing goes here
+			}
+		});
+	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btn_AG_Submit:
 			selectedName = etName.getText().toString();
-			selectedNum = Long.valueOf(etGradeNum.getText().toString());
-			selectedDen = Long.valueOf(etGradeDen.getText().toString());
-			finalGrade = selectedNum / selectedDen;
+			numGrade = Long.valueOf(etGradeNum.getText().toString());
+			max_grade = Long.valueOf(etGradeDen.getText().toString());
+			courseID = course.getId();
+			typeID = type.getId();
 			
-			//TODO FINISH THIS SHIT.
-	        Grade g = gradeSource.createGrade(7, 5, 100, "Final Exam", 95);
+			// Add the grade to the database
+			gradeSource.open();
+			gradeSource.createGrade(typeID, courseID, max_grade, selectedName, numGrade);
+			gradeSource.close();
 			break;
+
 		case R.id.btn_AG_Cancel:
 			finish();
 			break;
-		}	
-	} 
-	
+		}
+	}
+
 	public void init() {
 		btnCancel = (Button) findViewById(R.id.btn_AG_Cancel);
 		btnSubmit = (Button) findViewById(R.id.btn_AG_Submit);
@@ -103,8 +117,10 @@ public class AddGradeActivity extends Activity implements OnClickListener{
 		etName = (EditText) findViewById(R.id.et_AG_name);
 		etGradeNum = (EditText) findViewById(R.id.et_AG_numirator);
 		etGradeDen = (EditText) findViewById(R.id.et_AG_denominator);
-		
+
 		btnCancel.setOnClickListener(this);
 		btnSubmit.setOnClickListener(this);
+		
+		gradeSource = new GradesDataSource(this);
 	}
 }
